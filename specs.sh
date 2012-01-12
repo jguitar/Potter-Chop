7116a12ccc416ba6133d0dc89593afa8a850189e
Juan F. Pérez <j.guitar@gmail.com>
2012-01-12 12:01:36 +0100

    Limpieza!

--- specs.sh
+++ specs.sh
@@ -1,5 +1,2 @@
 clear
-rspec spec/book_order_spec.rb -c
-rspec spec/order_db_spec.rb -c
-rspec spec/db_connection_spec.rb -c
-rspec spec/shopping_basquet_spec.rb -c
+rspec spec/price_calculation_spec.rb -c
--- src/order_db.rb
+++
@@ -1,8 +0,0 @@
-class Order_db
-  
-  def initilize(db_conn)
-    @db_conn = db_conn
-  end
-  
-  
-end

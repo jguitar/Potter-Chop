fa5397e0d2b77b774a6ddd85639fbe2eb7961a97
Juan F. Pérez <j.guitar@gmail.com>
2012-01-18 12:10:47 +0100

    Refactorización para introducir una clase que gestione la base de datos.

--- specs.sh
+++ specs.sh
@@ -1,3 +1,5 @@
 clear
 rspec spec/price_calculation_spec.rb -c
-rspec spec/book_store_spec.rb -c
+rspec spec/order_store_spec.rb -c
+rspec spec/order_factory_spec.rb -c
+rspec spec/order_database_spec.rb -c

87c6e01d6d174722210b29d49d1790375f77a2a9
Juan F. Pérez <j.guitar@gmail.com>
2012-02-09 12:37:34 +0100

    Limpieza de viws antiguos y renombrado del index. Limpieza de las rutas de sinatra y el API Rest

--- app.rb
+++ app.rb
@@ -4,9 +4,7 @@ require './models/order'
 require './models/order_store'
 
 get '/' do
-  order_store = OrderStore.new
-  @list = order_store.order_list
-  erb :index_new
+  erb :index
 end
 
 get '/api/price' do
@@ -28,13 +26,8 @@ end
 post '/api/order' do
   order = Order.new(params['books'], params['name'], params['address'])
   if order.valid? then
-    @price = order.get_price
-    
     order_store = OrderStore.new
     order_store.new_order(order.books, order.name, order.address)
-    
-    erb :order_ok
-  else
-    erb :order_ko
   end
+  return ""
 end
